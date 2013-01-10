06a0e642e94c82ce4380cf20a5b26c9b0ec60ece
matt <>
2013-01-10 15:18:22 +0000

    Replace with DIRECTORY_SEPARATOR in autoload

--- App.php
+++ App.php
@@ -6,7 +6,7 @@ class App
 {
     protected static function autoload($className)
     {
-        $classPath = __DIR__."/../../".str_replace("\\", "/", $className).".php";
+        $classPath = __DIR__."/../../".str_replace("\\", DIRECTORY_SEPARATOR, $className).".php";
         // Do nothing if the file does not exist to allow class_exists etc. to work as expected
         if (file_exists($classPath)) {
             require($classPath);
