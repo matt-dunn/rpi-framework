55942746ddb86ee3e19193c5f9969667bcf72462
matt <>
2013-01-14 12:09:44 +0000

    Hook onto component edit events to support image captions

--- View/Js/Document/style.js
+++ View/Js/Document/style.js
@@ -10,25 +10,60 @@
  */
 
 RPI._("framework.view.document").style = (function() {
-    jQuery(document).ready(
-        function() {
-            jQuery(".document img[title]").each(
-                function() {
-                    var element = jQuery(this);
-                    var captionClassName = "c-caption-right";
-                    if(element.hasClass("right")) {
-                        captionClassName = "c-caption-right";
-                    } else if(element.hasClass("left")) {
-                        captionClassName = "c-caption-left";
-                    }
-
-                    element.wrap("<span class=\"c-caption " + captionClassName + "\"/>");
-                    element.parent().append("<span>" + element.attr("title") + "</span>");
-                    element.removeClass("left");
-                    element.removeClass("right");
-                    element.parent().width(element.width());
-                }
-            );
+//    jQuery(document).live(
+//        "beforeload.RPI.component.edit",
+//        function(e, component, option) {
+//            console.log(e.type)
+//        }
+//    );
+        
+    jQuery(document).live(
+        "load.RPI.component.edit",
+        function(e, component, option) {
+            insertCaptions(jQuery(component).find(".document img[title]"));
         }
     );
+        
+    jQuery(document).live(
+        "beforesave.RPI.component.edit",
+        function(e, component, contentElement, option) {
+            removeCaptionsAndUpdateImageAttributes(contentElement.find("img[title]"));
+//            e.preventDefault();
+        }
+    );
+        
+        
+        
+    function insertCaptions(elements) {
+        jQuery(elements).each(
+            function() {
+                var element = jQuery(this);
+                var captionClassName = "c-caption-right";
+                if(element.hasClass("right")) {
+                    captionClassName = "c-caption-right";
+                } else if(element.hasClass("left")) {
+                    captionClassName = "c-caption-left";
+                }
+
+                element.wrap("<span class=\"c-caption " + captionClassName + "\"/>");
+                element.parent().append("<span>" + element.attr("title") + "</span>");
+                element.removeClass("left");
+                element.removeClass("right");
+                element.parent().width(element.width());
+            }
+        );
+    }
+    
+    function removeCaptionsAndUpdateImageAttributes(elements) {
+        jQuery(elements).each(
+            function() {
+                var element = jQuery(this);
+                var container = element.parents(".c-caption:first");
+                var title = container.find("span").text();
+                
+                element.attr("title", title);
+                container.replaceWith(element);
+            }
+        );
+    }
 })();
