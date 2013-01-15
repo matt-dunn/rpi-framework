3aa6785f336470990721b7bcd7ac0429d3fb250d
matt <>
2013-01-15 20:18:13 +0000

    Setup functions before init

--- Bootstrap.php
+++ Bootstrap.php
@@ -29,7 +29,7 @@ if (!isset($GLOBALS["RPI_FRAMEWORK_CACHE_ENABLED"])) {
 // ================================================================================================================
 // Application initialisation
 
+require("Functions.php");
+
 require(__DIR__."/App.php");
 \RPI\Framework\App::init();
-
-require("Functions.php");
