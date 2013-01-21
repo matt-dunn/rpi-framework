e185a8cf5c920ca7e4f28828eea0b4b5b5123f87
matt <>
2013-01-21 16:47:55 +0000

    Fixed service insterface abstracts

--- Services/IService.php
+++ Services/IService.php
@@ -4,5 +4,10 @@ namespace RPI\Framework\Services;
 
 interface IService
 {
+    /**
+     * Clear the service instance - used for unit testing only
+     */
+    public static function clearInstance();
+    
     public static function getInstance();
 }
