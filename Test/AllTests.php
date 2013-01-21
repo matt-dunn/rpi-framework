d6785c451d2b493ff8263c116f9cc96f3da76604
matt <>
2013-01-21 17:40:13 +0000

    phpunit 	- created phpunit configs at each level 	- updated alltests.php for IDE

--- Test/AllTests.php
+++ Test/AllTests.php
@@ -6,18 +6,26 @@ class AllTests
 {
     public static function suite()
     {
-        $suite = new \PHPUnit_Framework_TestSuite('RPI_Framework');
-
-        $files = array();
-        $basePath = realpath(dirname(__FILE__)."/../../");
+        $rootPath = realpath(__DIR__."/../../../");
+        $basePath = realpath(__DIR__."/../../../");
+        
         \RPI\Framework\Helpers\FileUtils::find($basePath, "/.*Test\.php$/", $files, true, false);
         $files = array_keys($files);
 
+        $suites = array();
+        
         foreach ($files as $file) {
-            $testClassName = "RPI".str_replace(".php", "", str_replace("/", "\\", substr($file, strlen($basePath))));
-            echo "Found test '$testClassName'\n";
+            $suiteName = str_replace(DIRECTORY_SEPARATOR, "\\", substr($basePath, strlen($rootPath) + 1));
+            if (isset($suites[$suiteName])) {
+                $suite = $suites[$suiteName];
+            } else {
+                $suite = new \PHPUnit_Framework_TestSuite($suiteName);
+                $suites[$suiteName] = $suite;
+            }
+            
+            $testClassName = $suiteName.str_replace(".php", "", str_replace("/", "\\", substr($file, strlen($basePath))));
+            echo "Found test '$testClassName' (suite: '$suiteName')\n";
             $suite->addTestSuite($testClassName);
-
         }
 
         return $suite;
