fd61bcaddaf3a2c779d43b7bfc4a7174c2618349
matt <>
2013-01-23 18:45:31 +0000

    Only render component if it has body content

--- Component/View/Php/View.php
+++ Component/View/Php/View.php
@@ -13,60 +13,62 @@ abstract class View extends \RPI\Framework\Controller\Message\View\Php\View impl
      */
     final public function render($model, \RPI\Framework\Controller $controller, array $options)
     {
-        $sectionAttributes = "";
-        $sectionOptionsHTML = "";
-        $className = trim(
-            "component ".$controller->safeTypeName." "
-            .$controller->options->className
-        );
-
-        if ($controller instanceof \RPI\Framework\Component) {
-            if ($controller->isDynamic || $controller->editable) {
-                $sectionAttributes .= " data-type=\"{$controller->getType()}\" data-id=\"{$controller->id}\"";
-            }
-            
-            if (isset($controller->service)) {
-                $sectionAttributes .= " data-service=\"{$controller->service}\"";
-            }
-            
-            foreach ($controller->options->get("data") as $name => $value) {
-                $sectionAttributes .= " data-".strtolower($name)."\"=\"{$value}\"";
-            }
-            
-            if ($controller->editable) {
-                $sectionOptionsHTML = "<ul class=\"options\">";
-                $className .= " component-editable";
-                if ($controller->editMode) {
-                    $sectionOptionsHTML .= <<<EOT
-                        <li data-option="save" class="d">
-                            Save
-                        </li>
-                        <li data-option="cancel" class="l" title ="Complete">
-                            X
-                        </li>
-EOT;
-                    $className .= " component-editmode";
-                } else {
-                    $sectionOptionsHTML .= "
-                        <li data-option=\"edit\" class=\"l\">
-                            Edit
-                        </li>";
-                }
-                $sectionOptionsHTML .= "</ul>";
-            }
-        }
-
-        $componentRendition = $this->renderComponentView($model, $controller, $options);
         $rendition = "";
 
-        if ($componentRendition !== false) {
-            $rendition = <<<EOT
-<section class="$className"$sectionAttributes>
-    {$sectionOptionsHTML}
+        $componentRendition = $this->renderComponentView($model, $controller, $options);
+        if ($componentRendition != "") {
+            $sectionAttributes = "";
+            $sectionOptionsHTML = "";
+            $className = trim(
+                "component ".$controller->safeTypeName." "
+                .$controller->options->className
+            );
 
-    {$componentRendition}
-</section>
+            if ($controller instanceof \RPI\Framework\Component) {
+                if ($controller->isDynamic || $controller->editable) {
+                    $sectionAttributes .= " data-type=\"{$controller->getType()}\" data-id=\"{$controller->id}\"";
+                }
+
+                if (isset($controller->service)) {
+                    $sectionAttributes .= " data-service=\"{$controller->service}\"";
+                }
+
+                foreach ($controller->options->get("data") as $name => $value) {
+                    $sectionAttributes .= " data-".strtolower($name)."\"=\"{$value}\"";
+                }
+
+                if ($controller->editable) {
+                    $sectionOptionsHTML = "<ul class=\"options\">";
+                    $className .= " component-editable";
+                    if ($controller->editMode) {
+                        $sectionOptionsHTML .= <<<EOT
+                            <li data-option="save" class="d">
+                                Save
+                            </li>
+                            <li data-option="cancel" class="l" title ="Complete">
+                                X
+                            </li>
 EOT;
+                        $className .= " component-editmode";
+                    } else {
+                        $sectionOptionsHTML .= "
+                            <li data-option=\"edit\" class=\"l\">
+                                Edit
+                            </li>";
+                    }
+                    $sectionOptionsHTML .= "</ul>";
+                }
+            }
+
+            if ($componentRendition !== false) {
+                $rendition = <<<EOT
+    <section class="$className"$sectionAttributes>
+        {$sectionOptionsHTML}
+
+        {$componentRendition}
+    </section>
+EOT;
+            }
         }
 
         return $rendition;
