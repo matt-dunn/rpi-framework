e08e51eef0166f5824bb391878e497079860a60d
matt <>
2013-01-25 14:48:07 +0000

    Tidied message

--- WebService/Exceptions/InvalidMimeType.php
+++ WebService/Exceptions/InvalidMimeType.php
@@ -7,8 +7,9 @@ class InvalidMimeType extends WebService
     public function __construct($requestMimeType)
     {
         parent::__construct(
-            "Invalid mime type '$requestMimeType'. 
-                Set correct CONTENT_TYPE on request or ensure there is an appropiate handler for this mime type."
+            "Invalid mime type '$requestMimeType'. ".
+            "Set correct CONTENT_TYPE on request or ensure there is an ".
+            "appropiate handler for this mime type."
         );
     }
 }
