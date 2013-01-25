68f3acd75e065ac8c2891628ea0cd700da9e3efe
matt <>
2013-01-25 18:52:27 +0000

    Added priority to set page title

--- Controller/HTML/Front.php
+++ Controller/HTML/Front.php
@@ -4,7 +4,23 @@ namespace RPI\Framework\Controller\HTML;
 
 abstract class Front extends \RPI\Framework\Controller\HTML
 {
+    /**
+     * Return the page title
+     */
     abstract public function getPageTitle();
     
-    abstract public function setPageTitle($title);
+    /**
+     * Set the page title
+     * 
+     * The priority must always be set to 100 for controllers marked as primaryController = true
+     * so all other controllers must therefore set the priotity to <= 99. Controllers which only
+     * wish to set the title as a default (i.e. if nothing else on the page has set a title)
+     * then the priority should be set to 0. Other controllers may set this to any other value
+     * between 1 and 99. Care must be taken to ensure there are no competing controllers are
+     * trying to set the priority to the same value on the same page.
+     * 
+     * @param string $title Page title
+     * @param int $priority Priority setting of setting the title. Value 0 to 100.
+     */
+    abstract public function setPageTitle($title, $priority = 0);
 }
