708a39cab76ed5c825639cc1245c9e151aaae6ee
matt <>
2013-02-01 09:49:21 +0000

    Do not store server error page titles

--- Controller/HTML/Front.php
+++ Controller/HTML/Front.php
@@ -86,7 +86,9 @@ abstract class Front extends \RPI\Framework\Controller\HTML
      * wish to set the title as a default (i.e. if nothing else on the page has set a title)
      * then the priority should be set to 0. Other controllers may set this to any other value
      * between 1 and 99. Care must be taken to ensure there are no competing controllers are
-     * trying to set the priority to the same value on the same page.
+     * trying to set the priority to the same value on the same page. Setting a priority of -1
+     * will only set the title for the current request so can be used if there is for example
+     * a server error.
      * 
      * @param string $title Page title
      * @param int $priority Priority setting of setting the title. Value 0 to 100.
@@ -99,7 +101,13 @@ abstract class Front extends \RPI\Framework\Controller\HTML
         
         $title = \RPI\Framework\Facade::localisation()->t("site.controller.page.title", array($title));
         
-        if (self::$pageTitleDetails["title"] != $title && $priority >= self::$pageTitleDetails["priority"]) {
+        if (self::$pageTitleDetails["title"] != $title
+            && ($priority >= self::$pageTitleDetails["priority"]) || $priority === -1) {
+            // Do not overwrite a request priority title
+            if ($priority !== -1 && self::$pageTitleDetails["priority"] === -1) {
+                return false;
+            }
+            
             self::$pageTitleDetails["title"] = $title;
             self::$pageTitleDetails["priority"] = (int)$priority;
 
@@ -107,26 +115,26 @@ abstract class Front extends \RPI\Framework\Controller\HTML
                 new \RPI\Framework\Events\PageTitleUpdated(array("title" => $title))
             );
             
-            $frontStore = \RPI\Framework\Helpers\Reflection::getDependency(
-                $GLOBALS["RPI_APP"],
-                null,
-                null,
-                "RPI\Framework\Cache\IFront"
-            );
-
-            if (!isset($frontStore)) {
-                throw new \Exception(
-                    "RPI\Framework\Cache\IFront dependency not configured correctly"
+            if ($priority !== -1) {
+                $frontStore = \RPI\Framework\Helpers\Reflection::getDependency(
+                    $GLOBALS["RPI_APP"],
+                    null,
+                    null,
+                    "RPI\Framework\Cache\IFront"
                 );
-            }
 
-            //if ($GLOBALS["RPI_FRAMEWORK_CACHE_ENABLED"] === true) {
+                if (!isset($frontStore)) {
+                    throw new \Exception(
+                        "RPI\Framework\Cache\IFront dependency not configured correctly"
+                    );
+                }
+
                 $frontStore->store(
                     \RPI\Framework\Helpers\HTTP::getUrlPath()."-title",
                     serialize(self::$pageTitleDetails),
                     "title"
                 );
-            //}
+            }
         
             return true;
         }
