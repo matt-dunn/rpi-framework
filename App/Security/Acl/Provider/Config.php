8e3f0a5e3b878507a24f362657d7585c25c37e0f
matt <>
2013-02-16 14:58:02 +0000

    Refactored config into base class and created schema for security config

--- App/Security/Acl/Provider/Config.php
+++ App/Security/Acl/Provider/Config.php
@@ -2,7 +2,7 @@
 
 namespace RPI\Framework\App\Security\Acl\Provider;
 
-class Config extends \RPI\Framework\App\Config implements \RPI\Framework\App\Security\Acl\Model\IProvider
+final class Config extends \RPI\Framework\App\Config\Base implements \RPI\Framework\App\Security\Acl\Model\IProvider
 {
     private $aceMap = array();
     
@@ -22,50 +22,10 @@ class Config extends \RPI\Framework\App\Config implements \RPI\Framework\App\Sec
         \RPI\Framework\Model\User $user
     ) {
         return false;
-        
-        // TODO: need to implement this storage...
-        switch ($user->email) {
-            case "full@rpi.co.uk":
-                switch ($domainObject->getId()) {
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2439":    // 403
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2438":    // 404
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2437":    // 500
-                    case "f10d5cc4-0003-480d-9618-3c3dfcdb2439":    // test
-                    case "f10d5cc4-0003-480d-9618-3c3dfcdb2439":    // test2
-                    case "a10d5cc4-1233-480d-9618-3c3dfcdb2439":    // complex-markup
-                    case "d90d51c4-1003-280d-3618-3c3dfcdb2438":    // navigation (default)
-                    case "d90d51c4-1003-280d-3618-3c3dfcdb2439":    // navigation (footer)
-                        return false;
-                }
-                break;
-            case "demo@rpi.co.uk":
-                switch ($domainObject->getId()) {
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2439":    // 403
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2438":    // 404
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2437":    // 500
-                    case "f10d5cc4-0003-480d-9618-3c3dfcdb2439":    // test
-                    case "f10d5cc4-0003-480d-9618-3c3dfcdb2439":    // test2
-                    case "a10d5cc4-1233-480d-9618-3c3dfcdb2439":    // complex-markup
-                    case "d90d51c4-1003-280d-3618-3c3dfcdb2438":    // navigation (default)
-                    case "d90d51c4-1003-280d-3618-3c3dfcdb2439":    // navigation (footer)
-                        return true;
-                }
-                break;
-            case "guest@rpi.co.uk":
-                switch ($domainObject->getId()) {
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2439":    // 403
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2438":    // 404
-                    case "f90d51c4-0003-480d-9618-3c3dfcdb2437":    // 500
-                    case "f10d5cc4-0003-480d-9618-3c3dfcdb2439":    // test
-                    case "f10d5cc4-0003-480d-9618-3c3dfcdb2439":    // test2
-                    case "a10d5cc4-1233-480d-9618-3c3dfcdb2439":    // complex-markup
-                    case "d90d51c4-1003-280d-3618-3c3dfcdb2438":    // navigation (default)
-                    case "d90d51c4-1003-280d-3618-3c3dfcdb2439":    // navigation (footer)
-                        return false;
-                }
-                break;
-        }
+    }
 
-        return false;
+    protected function getSchema()
+    {
+        return __DIR__."/../../../../../Schemas/Conf/Security.1.0.0.xsd";
     }
 }
