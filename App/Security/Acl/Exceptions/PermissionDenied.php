f2299c622abf26e4d6a60832f29cf5944a40921c
matt <>
2013-02-16 18:57:20 +0000

    Throw acl permission denied exception to provide better logging information

---
+++ App/Security/Acl/Exceptions/PermissionDenied.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace RPI\Framework\App\Security\Acl\Exceptions;
+
+class PermissionDenied extends \RPI\Framework\Exceptions\Forbidden implements \RPI\Framework\Exceptions\IException
+{
+    /**
+     * 
+     * @param int $access       \RPI\Framework\App\Security\Acl\Model\IAcl constant
+     * @param \RPI\Framework\App\Security\Acl\Model\IDomainObject $object
+     * @param \Exception $previous
+     */
+    public function __construct(
+        $access,
+        \RPI\Framework\App\Security\Acl\Model\IDomainObject $object = null,
+        $propertyName = null,
+        \Exception $previous = null
+    ) {
+        parent::__construct($previous);
+        
+        $permission = array();
+        
+        if ($access & \RPI\Framework\App\Security\Acl\Model\IAcl::CREATE) {
+            $permission[] = "CREATE";
+        }
+        if ($access & \RPI\Framework\App\Security\Acl\Model\IAcl::READ) {
+            $permission[] = "READ";
+        }
+        if ($access & \RPI\Framework\App\Security\Acl\Model\IAcl::UPDATE) {
+            $permission[] = "UPDATE";
+        }
+        if ($access & \RPI\Framework\App\Security\Acl\Model\IAcl::DELETE) {
+            $permission[] = "DELETE";
+        }
+        
+        $this->message =
+            "Permission denied accessing '".
+            get_class($object).
+            "' (".implode(", ", $permission).(isset($propertyName) ? ":$propertyName" : "").
+            ")";
+    }
+}
