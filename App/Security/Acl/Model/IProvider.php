cac9fba1b2f9b17ce65e8cff3e214ce1e7a3fe07
matt <>
2013-02-16 15:19:48 +0000

    Added phpdoc

--- App/Security/Acl/Model/IProvider.php
+++ App/Security/Acl/Model/IProvider.php
@@ -4,8 +4,21 @@ namespace RPI\Framework\App\Security\Acl\Model;
 
 interface IProvider
 {
+    /**
+     * 
+     * @param string $objectType
+     * 
+     * @return array
+     */
     public function getAce($objectType);
     
+    /**
+     * 
+     * @param \RPI\Framework\App\Security\Acl\Model\IDomainObject $domainObject
+     * @param \RPI\Framework\Model\User $user
+     * 
+     * @return boolean
+     */
     public function isOwner(
         \RPI\Framework\App\Security\Acl\Model\IDomainObject $domainObject,
         \RPI\Framework\Model\User $user
