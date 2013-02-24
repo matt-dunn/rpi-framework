69dc67d9acdbf4e49ded9876633609d2159b804c
matt <>
2013-02-24 15:33:13 +0000

    Added getClassPath method

--- Autoload.php
+++ Autoload.php
@@ -6,11 +6,16 @@ class Autoload
 {
     private static function autoload($className)
     {
-        $classPath = __DIR__."/../../".str_replace("\\", DIRECTORY_SEPARATOR, $className).".php";
+        $classPath = self::getClassPath($className);
         // Do nothing if the file does not exist to allow class_exists etc. to work as expected
         if (file_exists($classPath)) {
             require($classPath);
         }
+    }
+    
+    public static function getClassPath($className)
+    {
+        return __DIR__."/../../".str_replace("\\", DIRECTORY_SEPARATOR, $className).".php";
     }
 
     public static function init()
